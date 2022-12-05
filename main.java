6e91cbb505e473daa6f69d2204227d5e7d6f7c1c
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2022-12-04 20:35:04 -0500

    fix main

--- main.java
+++ main.java
@@ -4,25 +4,56 @@ public class main {
     public static void main (String[] args){
         //array conditions
         int rowQuantity = 6;
-        int columnQuantity = 6;
+        int columnQuantity = 10;
         //makes the rows have a random length between 1 and the columnQuantity value
         boolean randomRowSize = true;
-
-        findConditions(populateMatrix(createMatrix(rowQuantity,columnQuantity, randomRowSize)));
+        findConditions(
+                populateMatrix(
+                        createMatrix(
+                                rowQuantity,
+                                columnQuantity,
+                                randomRowSize
+                        )
+                )
+        );
     }
 
     //find the number in the array that is the LARGEST in its row but SMALLEST in its column,
     //or it might be the other way around idk
-    //return -1 if no such number exists
+    //print -1 if no such number exists
 
-    public static int findConditions(int[][] ints){
+    public static void findConditions(int[][] matrix){
         int solution = -1;
-        //write your code here
+        //write your code below
+        int l = 0;
+        for(int row = 0; row < matrix.length; row++){
+            l = matrix[row].length;
+        }
+        int[] pointer = new int[2];
+        int[] largestInColumn = new int[l];
+        for(int i = 0; i < l;i++){
+            largestInColumn[i] = 0;
+        }
 
+        //find the largest in every row
+        for(int row = 0; row < matrix.length; row++){
+            pointer[0] = 0;
+            for(int column = 0; column < matrix[row].length; column++){
+                if (matrix[row][column] > pointer[0]){
+                    pointer[0] = matrix[row][column];
+                    pointer[1] = column;
+                }
+                if (largestInColumn[column] < matrix[row][column]){
+                    largestInColumn[column] = matrix[row][column];
+                }
+            }
+            System.out.println(pointer[0] + "" + pointer[1]);
+        }
+        for(int i = 0; i < largestInColumn.length;i++){
+            System.out.print(largestInColumn[i]);
+        }
 
-
-
-        return solution;
+        System.out.print(solution);
     }
 
 
@@ -44,11 +75,24 @@ public class main {
     public static int[][] populateMatrix(int matrix[][]){
         for(int row = 0; row < matrix.length; row++){
             for(int column = 0; column < matrix[row].length; column++){
-                matrix[row][column] = (int)(Math.random()*10+1);
+                matrix[row][column] = (int)(Math.random()*9+1);
             }
         }
-
+        printMatrix(matrix);
         return matrix;
     }
+
+    public static void printMatrix(int matrix[][]){
+        System.out.print("The Matrix looks like: \n");
+        for(int row = 0; row < matrix.length; row++){
+            for(int column = 0; column < matrix[row].length; column++){
+                System.out.print(matrix[row][column]);
+                System.out.print('\t');
+            }
+            System.out.print('\n');
+        }
+        System.out.print("\nThe result of query is: \n");
+    }
+
 
 }
